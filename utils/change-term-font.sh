b40866b894a5aae34674f7f5345d71f5850ec517
Paul Johnson <paul@pjcj.net>
2015-02-21 02:27:39 +0100

    Add change-term-font.sh.

---
+++ utils/change-term-font.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Written by Yu-Jie Lin
+
+FONTNAME='Envy Code R'
+FONTNAME='Inconsolata for Powerline'
+FONTSIZE='16'
+FONTSTYLE='Regular'
+
+while getopts "f:s:S:" opt; do
+  case $opt in
+    f)
+      FONTNAME="$OPTARG"
+      ;;
+    s)
+      FONTSIZE="$OPTARG"
+      ;;
+    S)
+      FONTSTYLE="$OPTARG"
+      ;;
+  esac
+done
+
+FONT="xft:$FONTNAME:style=$FONTSTYLE:size=$FONTSIZE:antialias=false"
+# FONT="xft:$FONTNAME:pizelsize=$FONTSIZE"
+
+echo setting font $FONT
+
+printf '\e]710;%s\007' "$FONT"
