60dd3f5f7fed6bc72a2cfb8b2a1dbd3cc2d2c422
Paul Johnson <paul@pjcj.net>
2017-03-29 10:08:38 +0200

    Improve shell template

--- templates/template.sh
+++ templates/template.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-IFS=$'\n\t'
+# set -x
+# IFS=$'\n\t'
 
 usage() {
     echo <<EOT
@@ -8,7 +9,9 @@ $0 --help
 EOT
     exit 0
 }
-expr "$*" : ".*--help" > /dev/null && usage
+verbose=0
+expr "$*" : ".*--help"    > /dev/null && usage
+expr "$*" : ".*--verbose" > /dev/null && verbose=1
 
 readonly LOG_FILE="/tmp/$(basename "$0").log"
 _p() { echo "$*" | tee -a "$LOG_FILE" >&2; }
@@ -20,18 +23,30 @@ pe() { _p "[ERROR]   $*";                  }
 pf() { _p "[FATAL]   $*"; exit 1;          }
 
 cleanup() {
-    pt "Cleaning up"
+    ((verbose)) && pi "Cleaning up"
     # Remove temporary files
     # Restart services
     # ...
 }
 
 main() {
-    pt "Starting"
+    ((verbose)) && pi "Running $*"
     # implementation
+    case "$1" in
+        options)
+            perl -nE 'say $1 =~ s/"//gr =~ s/\s*\|\s*/\n/gr if /^ {8}"?([a-zA-Z0-9_ "|\\]+)"?(?:\)|\s*\|\s*\\)$/ && $1 !~ /^_/' < "$0"
+            ;;
+        *)
+            echo Unknown option "$1"
+            ;;
+    esac
 }
 
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
     trap cleanup EXIT
-    main
+    main "$@"
 fi
+
+# For zsh completion:
+# _%FILE%() { reply=($(%FILE% options)) }
+# compctl -K _%FILE% %FILE%
