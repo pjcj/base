1e06fc241371b5770249a6c386984690e5a132a1
Paul Johnson <paul@pjcj.net>
2017-04-07 13:50:40 +0200

    Update sh template

--- templates/template.sh
+++ templates/template.sh
@@ -13,37 +13,38 @@ verbose=0
 expr "$*" : ".*--help"    > /dev/null && usage
 expr "$*" : ".*--verbose" > /dev/null && verbose=1
 
-readonly LOG_FILE="/tmp/$(basename "$0").log"
-_p() { echo "$*" | tee -a "$LOG_FILE" >&2; }
-pt() { _p "[TRACE]   $*";                  }
-pd() { _p "[DEBUG]   $*";                  }
-pi() { _p "[INFO]    $*";                  }
-pw() { _p "[WARNING] $*";                  }
-pe() { _p "[ERROR]   $*";                  }
-pf() { _p "[FATAL]   $*"; exit 1;          }
+script=$(basename "$0")
+dir=$(dirname "$0")
+readonly LOG_FILE="/tmp/$script.log"
+_p() { l=$1; shift; echo "$l: $script $*" | tee -a "$LOG_FILE" >&2; }
+pt() { _p "[TRACE]  " "$*";                                         }
+pd() { _p "[DEBUG]  " "$*";                                         }
+pi() { _p "[INFO]   " "$*";                                         }
+pw() { _p "[WARNING]" "$*";                                         }
+pe() { _p "[ERROR]  " "$*";                                         }
+pf() { _p "[FATAL]  " "$*"; exit 1;                                 }
 
 cleanup() {
+    declare -r res=$?
     ((verbose)) && pi "Cleaning up"
-    # Remove temporary files
-    # Restart services
-    # ...
+    exit $res
 }
 
 main() {
     ((verbose)) && pi "Running $*"
-    # implementation
     case "$1" in
+        # implementation
         options)
             perl -nE 'say $1 =~ s/"//gr =~ s/\s*\|\s*/\n/gr if /^ {8}"?([a-zA-Z0-9_ "|\\]+)"?(?:\)|\s*\|\s*\\)$/ && $1 !~ /^_/' < "$0"
             ;;
         *)
-            echo Unknown option "$1"
+            pf "Unknown option: $1"
             ;;
     esac
 }
 
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
-    trap cleanup EXIT
+    trap cleanup EXIT INT
     main "$@"
 fi
 
