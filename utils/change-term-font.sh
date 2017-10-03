964b8ba89426e84f80f0293d3ad32976b6ac91cf
Paul Johnson <paul@pjcj.net>
2017-10-03 23:56:21 +0200

    Keep shellcheck happy

--- utils/change-term-font.sh
+++ utils/change-term-font.sh
@@ -25,6 +25,6 @@ done
 FONT="xft:$FONTNAME:style=$FONTSTYLE:size=$FONTSIZE:antialias=false"
 # FONT="xft:$FONTNAME:pizelsize=$FONTSIZE"
 
-echo setting font $FONT
+echo setting font "$FONT"
 
 printf '\e]710;%s\007' "$FONT"
