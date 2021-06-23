7dcbae80bfe5db36d08704aec51ae8402b209543
Paul Johnson <paul@pjcj.net>
2021-06-23 23:07:59 +0200

    Add more telescope mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,12 +4,10 @@ local g    = vim.g          -- a table to access global variables
 local opt  = vim.opt        -- set options
 local api  = vim.api        -- vim api
 local exec = api.nvim_exec  -- execute nvim
+local map  = vim.api.nvim_set_keymap
+local def  = { noremap = true, silent = true }
 
 g.mapleader = ","
-
-local map = vim.api.nvim_set_keymap
-
-local def = { noremap = true, silent = true}
 
 map("i", "<F5>",       "[",   def)
 map("n", "r<F5>",      "r[",  def)
@@ -29,10 +27,13 @@ map("n", "<PageDown>", "0", def)
 
 map("n", "<leader>.",  [[<cmd>lua require"telescope.builtin".find_files({ hidden = true })<cr>]], def)
 map("n", "<leader> ",  [[<cmd>lua require"telescope.builtin".oldfiles()<cr>]],                    def)
-map("n", "<leader>m",  [[<cmd>lua require"telescope.builtin".buffers()<cr>]],                     def)
+map("n", "<leader>m",  [[<cmd>lua require"telescope.builtin".git_status()<cr>]],                  def)
+map("n", "<leader>fb", [[<cmd>lua require"telescope.builtin".buffers()<cr>]],                     def)
 map("n", "<leader>ff", [[<cmd>lua require"telescope.builtin".builtin()<cr>]],                     def)
 map("n", "<leader>fg", [[<cmd>lua require"telescope.builtin".live_grep()<cr>]],                   def)
 map("n", "<leader>fh", [[<cmd>lua require"telescope.builtin".help_tags()<cr>]],                   def)
+map("n", "<leader>fs", [[<cmd>lua require"telescope.builtin".grep_string()<cr>]],                 def)
+map("n", "<leader>fl", [[<cmd>lua require"telescope.builtin".current_buffer_fuzzy_find()<cr>]],   def)
 
 map("n", "<leader>gg", [[<cmd>tab Git commit<cr>]], def)
 
