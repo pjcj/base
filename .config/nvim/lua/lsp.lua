69fde7e2e2ce911f00e7813ee265c93bd99329d6
Paul Johnson <paul@pjcj.net>
2021-06-23 23:03:54 +0200

    Add lsp config
    The virtual_text config is on by default anyway but leave this here as a
    template.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -80,6 +80,13 @@ local function make_config()
     capabilities = capabilities,
     -- map buffer local keybindings when the language server attaches
     on_attach = on_attach,
+    handlers = {
+      ["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+          virtual_text = true -- enable virtual_text
+        }
+      ),
+    }
   }
 end
 
