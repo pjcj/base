bd7c2ac90b483a04a45501d84e51ace9d13c38bd
Paul Johnson <paul@pjcj.net>
2021-06-26 14:53:53 +0200

    Map tab in completion

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -148,6 +148,50 @@ require "compe".setup {
   },
 }
 
+local t = function(str)
+  return Api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+  local col = Fn.col(".") - 1
+  if col == 0 or Fn.getline("."):sub(col, col):match("%s") then
+    return true
+  else
+    return false
+  end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if Fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif Fn.call("vsnip#available", {1}) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return Fn["compe#complete"]()
+  end
+end
+
+_G.s_tab_complete = function()
+  if Fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif Fn.call("vsnip#jumpable", {-1}) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
+  else
+    -- If <S-Tab> is not working in your terminal, change it to <C-h>
+    return t "<S-Tab>"
+  end
+end
+
+Map("i", "<Tab>",   "v:lua.tab_complete()",   {expr = true})
+Map("s", "<Tab>",   "v:lua.tab_complete()",   {expr = true})
+Map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+Map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
 require "gitsigns".setup {
   signs = {
     add          = { show_count = true, text = "+" },
