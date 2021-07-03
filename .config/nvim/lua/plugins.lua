212bb49a9ad91434bbda769f6e01e89d067e646e
Paul Johnson <paul@pjcj.net>
2021-07-03 15:53:38 +0200

    Add nvim-scrollview

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -56,6 +56,7 @@ require("packer").startup(function(use)
   use "lfv89/vim-interestingwords"
   use "junegunn/vim-easy-align"
   use "tpope/vim-surround"
+  use "dstein64/nvim-scrollview"
 end)
 
 require "nvim-treesitter.configs".setup {
