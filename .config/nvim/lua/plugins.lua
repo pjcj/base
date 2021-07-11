b9a7c08ebf3af6748461a8f5cbc08201afb1fb01
Paul Johnson <paul@pjcj.net>
2021-07-12 00:03:46 +0200

    Move interestingwords into packer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -333,24 +333,28 @@ require("packer").startup(function(use)
     end,
   }
 
-  use "lfv89/vim-interestingwords"
+  use {
+    "lfv89/vim-interestingwords",
+    config = function()
+      G.interestingWordsGUIColors = {
+        '#72b5e4', '#f0c53f', '#ff8784', '#c5c7f1',
+        '#c2d735', '#78d3cc', '#ea8336', '#e43542',
+        '#ebab35', '#ebe735', '#aadd32', '#dcca6b',
+        '#219286', '#2f569c', '#ffb577', '#5282a4',
+        '#edfccf', '#67064c', '#f5bca7', '#95c474',
+        '#dece83', '#de9783', '#f2e700', '#e9e9e9',
+        '#69636d', '#626b98', '#f5f5a7', '#dcca6b',
+        '#b72a83', '#6f2b9d', '#69636d', '#5f569c',
+      }
+    end,
+  }
+
   use "junegunn/vim-easy-align"
   use "tpope/vim-surround"
   use "tpope/vim-repeat"
   use "dstein64/nvim-scrollview"
   use "edluffy/specs.nvim"
 end)
-
-G.interestingWordsGUIColors = {
-  '#72b5e4', '#f0c53f', '#ff8784', '#c5c7f1',
-  '#c2d735', '#78d3cc', '#ea8336', '#e43542',
-  '#ebab35', '#ebe735', '#aadd32', '#dcca6b',
-  '#219286', '#2f569c', '#ffb577', '#5282a4',
-  '#edfccf', '#67064c', '#f5bca7', '#95c474',
-  '#dece83', '#de9783', '#f2e700', '#e9e9e9',
-  '#69636d', '#626b98', '#f5f5a7', '#dcca6b',
-  '#b72a83', '#6f2b9d', '#69636d', '#5f569c',
-}
 
 G.scrollview_winblend = 75
 G.scrollview_column   = 1
