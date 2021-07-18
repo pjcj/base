a8340383a9b4958b6593e279632ebaea76d2ac4c
Paul Johnson <paul@pjcj.net>
2021-07-18 14:32:31 +0200

    Simplify init

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -5,8 +5,8 @@ Opt    = vim.opt                           -- set options
 Lopt   = vim.opt_local                     -- set local options
 Api    = vim.api                           -- vim api
 Exec   = Api.nvim_exec                     -- execute nvim
-Map    = vim.api.nvim_set_keymap           -- global mappings
-Bmap   = vim.api.nvim_buf_set_keymap       -- local mappings
+Map    = Api.nvim_set_keymap               -- global mappings
+Bmap   = Api.nvim_buf_set_keymap           -- local mappings
 Defmap = { noremap = true, silent = true } -- default map options
 
 require "colour"
