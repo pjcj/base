428e52783157141f13cdc646efaacad19b5c6211
Paul Johnson <paul@pjcj.net>
2021-09-04 21:15:11 +0200

    Compile packer on changing plugins file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,6 +6,13 @@ if Fn.empty(Fn.glob(install_path)) > 0 then
   Cmd "packadd packer.nvim"
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 require("packer").startup(function(use)
   -- Packer can manage itself
   use "wbthomason/packer.nvim"
