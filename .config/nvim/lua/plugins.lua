cc8e3813b261dfb6f8f4fbdfd6fb4fe9d6eae94e
Paul Johnson <paul@pjcj.net>
2021-10-30 18:49:20 +0200

    Rework completion setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -172,8 +172,6 @@ require("packer").startup(function(use)
     end,
   }
 
-  -- use { "andersevenrud/compe-tmux", branch = "cmp" }
-
   use {
     "hrsh7th/nvim-cmp",
     requires = {
@@ -187,46 +185,105 @@ require("packer").startup(function(use)
       "hrsh7th/cmp-emoji",
       "quangnguyen30192/cmp-nvim-tags",
       { "andersevenrud/compe-tmux", branch = "cmp" },
+      "onsails/lspkind-nvim",
     },
     config = function()
       require "cmp_nvim_lsp".setup()
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require "cmp_nvim_lsp".update_capabilities(capabilities)
       local cmp = require "cmp"
-      -- local lspkind = require("lspkind")
+      local lspkind = require "lspkind"
+
+      lspkind.init({
+        -- symbol_map = {
+        --     Text = "",
+        --     Method = "",
+        --     Function = "",
+        --     Constructor = "",
+        --     Field = "ﰠ",
+        --     Variable = "",
+        --     Class = "ﴯ",
+        --     Interface = "",
+        --     Module = "",
+        --     Property = "ﰠ",
+        --     Unit = "塞",
+        --     Value = "",
+        --     Enum = "",
+        --     Keyword = "",
+        --     Snippet = "",
+        --     Color = "",
+        --     File = "",
+        --     Reference = "",
+        --     Folder = "",
+        --     EnumMember = "",
+        --     Constant = "",
+        --     Struct = "פּ",
+        --     Event = "",
+        --     Operator = "",
+        --     TypeParameter = ""
+        -- }
+      })
+
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and
+          vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(
+          vim.api.nvim_replace_termcodes(key, true, true, true), mode, true
+        )
+      end
+
       cmp.setup {
         formatting = {
-          format = function(entry, vim_item)
-            vim_item.menu = ({
-              nvim_lsp    = '[LS]',
-              nvim_lua    = '[LL]',
-              path        = '[DIR]',
-              vsnip       = '[SNP]',
-              buffer      = '[BUF]',
-              cmp_tabnine = '[TN]',
-              tmux        = '[TMUX]',
-            })[entry.source.name]
-
-            -- if entry.source.name == 'nvim_lsp' then
-              -- vim_item.dup = 0
-            -- end
-
-            return vim_item
-          end
+          format = lspkind.cmp_format {
+            -- with_text = false,
+            -- maxwidth = 50,
+            menu = {
+              buffer   = "buf",
+              nvim_lsp = "lsp",
+              nvim_lua = "lua",
+              path     = "path",
+              tmux     = "tmux",
+              tags     = "tags",
+              vsnip    = "snip",
+              calc     = "calc",
+              spell    = "spell",
+              emoji    = "emoji",
+            }
+          },
         },
+        experimental = {native_menu = false, ghost_text = false},
         min_length = 0, -- allow for `from package import _` in Python
         mapping = {
-          ["<S-Tab>"]   = cmp.mapping.select_prev_item(),
-          ["<Tab>"]     = cmp.mapping.select_next_item(),
           ["<C-p>"]     = cmp.mapping.select_prev_item(),
           ["<C-n>"]     = cmp.mapping.select_next_item(),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"]     = cmp.mapping.close(),
-          -- This is handled by nvim-autopairs.
-          ["<CR>"] = cmp.mapping.confirm {
+          ["<CR>"]      = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true
-          }
+          },
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            -- elseif vim.fn["vsnip#available"](1) == 1 then
+              -- feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, {"i", "s"}),
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            -- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              -- feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, {"i", "s"})
         },
         sources = {
           { name = "nvim_lsp" },
@@ -253,7 +310,20 @@ require("packer").startup(function(use)
             require "luasnip".lsp_expand(args.body)
           end,
         },
+        -- snippet = {
+          -- expand = function(args)
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          -- end
+        -- },
       }
+
+      -- Use buffer source for `/`.
+      cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
+
+      -- Use cmdline & path source for ':'.
+      cmp.setup.cmdline(":", {
+        sources = cmp.config.sources({{name = "path"}}, {{name = "cmdline"}})
+      })
     end,
   }
 
