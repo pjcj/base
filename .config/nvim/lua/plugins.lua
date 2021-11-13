59339483cf9d8e17e5332398b7a06adde6f79fb1
Paul Johnson <paul@pjcj.net>
2021-11-13 13:59:35 +0100

    Configure cmp

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -263,14 +263,14 @@ require("packer").startup(function(use)
         experimental = {native_menu = false, ghost_text = true},
         min_length = 1,
         mapping = {
-          ["<C-p>"]     = cmp.mapping.select_prev_item(),
-          ["<C-n>"]     = cmp.mapping.select_next_item(),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"]     = cmp.mapping.close(),
-          ["<CR>"]      = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true
-          },
+          ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          -- ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
@@ -281,14 +281,14 @@ require("packer").startup(function(use)
             else
               fallback()
             end
-          end, {"i", "s"}),
+          end, { "i", "s" }),
           ["<S-Tab>"] = cmp.mapping(function()
             if cmp.visible() then
               cmp.select_prev_item()
             -- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
               -- feedkey("<Plug>(vsnip-jump-prev)", "")
             end
-          end, {"i", "s"})
+          end, { "i", "s" })
         },
         sources = {
           { name = "nvim_lsp" },
