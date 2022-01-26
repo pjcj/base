d8611a66125343e436cbe5977159b964d8861f7d
Paul Johnson <paul@pjcj.net>
2022-01-26 11:38:05 +0100

    Update gitsigns config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -397,39 +397,13 @@ require("packer").startup(function(use)
           topdelete    = { show_count = true, text = "‾" },
           changedelete = { show_count = true, text = "~" },
         },
-        numhl   = false,
-        linehl  = false,
-        keymaps = {
-          -- Default keymap options
-          noremap = true,
-          buffer  = true,
-
-          ["n <F2>"] = { expr = true, [[&diff ? '[c' : '<cmd>lua require "gitsigns.actions".prev_hunk()<CR>']]},
-          ["n <F3>"] = { expr = true, [[&diff ? ']c' : '<cmd>lua require "gitsigns.actions".next_hunk()<CR>']]},
-
-          ["n <leader>hs"] = '<cmd>lua require "gitsigns".stage_hunk()<CR>',
-          ["v <leader>hs"] = '<cmd>lua require "gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-          ["n <leader>hu"] = '<cmd>lua require "gitsigns".undo_stage_hunk()<CR>',
-          ["n <leader>hr"] = '<cmd>lua require "gitsigns".reset_hunk()<CR>',
-          ["v <leader>hr"] = '<cmd>lua require "gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-          ["n <leader>hR"] = '<cmd>lua require "gitsigns".reset_buffer()<CR>',
-          ["n <leader>hp"] = '<cmd>lua require "gitsigns".preview_hunk()<CR>',
-          ["n <leader>hb"] = '<cmd>lua require "gitsigns".blame_line{full = true}<CR>',
-
-          ["n <F1>"] = '<cmd>lua require "gitsigns".stage_hunk()<CR>',
-          ["v <F1>"] = '<cmd>lua require "gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-
-          -- Text objects
-          ["o ih"] = ':<C-U>lua require "gitsigns.actions".select_hunk()<CR>',
-          ["x ih"] = ':<C-U>lua require "gitsigns.actions".select_hunk()<CR>'
-        },
+        numhl                   = false,
+        linehl                  = false,
         watch_gitdir            = { interval = 1000 },
         sign_priority           = 6,
         update_debounce         = 100,
         status_formatter        = nil, -- Use default
-        diff_opts               = {
-          internal = true, -- If luajit is present
-        },
+        diff_opts               = { internal = true },  -- If luajit is present
         word_diff               = true,
         current_line_blame      = true,
         current_line_blame_opts = {
@@ -449,6 +423,37 @@ require("packer").startup(function(use)
           [9]   = "₉",
           ["+"] = "₊",
         },
+        on_attach = function(bufnr)
+          local function map(mode, lhs, rhs, opts)
+              opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+              vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+          end
+
+          -- Navigation
+          map('n', '<F3>', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+          map('n', '<F2>', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+
+          -- Actions
+          map('n', '<F1>', '<cmd>Gitsigns stage_hunk<CR>')
+          map('v', '<F1>', '<cmd>Gitsigns stage_hunk<CR>')
+          map('n', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+          map('v', '<leader>hs', '<cmd>Gitsigns stage_hunk<CR>')
+          map('n', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
+          map('v', '<leader>hr', '<cmd>Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+          map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+          map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+          map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+          map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+          map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+          map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+          map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+          map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+          -- Text object
+          map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end,
       }
     end,
   }
