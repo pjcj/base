0b14d5de6569d07439e290c8c1cabec08bcbe492
Paul Johnson <paul@pjcj.net>
2022-02-16 13:51:15 +0100

    Remove obsolete virtual_text config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -145,14 +145,6 @@ local function make_config()
     capabilities = capabilities,
     -- map buffer local keybindings when the language server attaches
     on_attach = on_attach,
-    handlers = {
-      ["textDocument/publishDiagnostics"] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics,
-        {
-          virtual_text = true, -- enable virtual_text
-        }
-      ),
-    },
   }
 end
 
