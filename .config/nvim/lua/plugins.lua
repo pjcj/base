fa3aedc03548e1a214575a099c8b0368f5178d3c
Paul Johnson <paul@pjcj.net>
2022-03-05 17:05:09 +0100

    Remove completion mapping on enter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -421,7 +421,7 @@ require("packer").startup(function(use)
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
           },
-          ["<CR>"] = cmp.mapping.confirm { select = true },
+          -- ["<CR>"] = cmp.mapping.confirm { select = true },
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
