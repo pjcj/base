836d147ede3a97b8a7d5a0883f6be544d20aeb53
Paul Johnson <paul@pjcj.net>
2023-02-04 16:51:01 +0100

    Reposition lsp signature box

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -19,8 +19,8 @@ local lsp_sig_cfg = {
   -- will set to true when fully tested, set to false will use whichever side has more space
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
-  floating_window_off_x = 1, -- adjust float windows x position.
-  floating_window_off_y = 1, -- adjust float windows y position.
+  floating_window_off_x = 0, -- adjust float windows x position.
+  floating_window_off_y = -4, -- adjust float windows y position.
 
 
   fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
