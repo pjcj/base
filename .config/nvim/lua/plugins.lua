6ffd4d9a3d638c8a885eb9acbff5ef45c05c1d09
Paul Johnson <paul@pjcj.net>
2023-02-18 23:15:33 +0100

    Disable cmp-cmdline whilst it's failing

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -776,14 +776,14 @@ packer.startup(function(use)
       })
 
       -- Use cmdline & path source for ':'.
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
+      -- cmp.setup.cmdline(":", {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = cmp.config.sources({
+      --     { name = "path" },
+      --   }, {
+      --     { name = "cmdline" },  -- currently broken
+      --   }),
+      -- })
     end,
   }
 
