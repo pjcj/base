2a88f308788f390b5346db05cf3e9f82f7f7537e
Paul Johnson <paul@pjcj.net>
2023-08-13 12:28:00 +0200

    Add yamllint

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -314,6 +314,7 @@ local plugins = {
         perl = { "codespell" },
         rst = { "codespell", "rstlint" },
         sh = { "codespell" },
+        yaml = { "codespell", "yamllint" },
       }
 
       local codespell_args = { "--builtin", "clear,rare,informal,usage,names" }
@@ -322,6 +323,13 @@ local plugins = {
         table.insert(codespell_args, vim.fn.getcwd() .. "/.codespell")
       end
       lint.linters.codespell.args = codespell_args
+
+      local yamllint_args = { }
+      if vim.fn.filereadable(".yamllint") == 1 then
+        table.insert(yamllint_args, "-c")
+        table.insert(yamllint_args, vim.fn.getcwd() .. "/.yamllint")
+      end
+      lint.linters.yamllint.args = yamllint_args
 
       local last_lint_time = 0
       vim.api.nvim_create_autocmd({
