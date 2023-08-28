3011b6d142365f827d95ea8f373a3287bc2207f0
Paul Johnson <paul@pjcj.net>
2023-08-28 23:44:24 +0200

    Add git messenger window border

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -901,6 +901,7 @@ local plugins = {
       vim.g.git_messenger_always_into_popup = 1
       vim.g.git_messenger_include_diff = "current"
       vim.g.git_messenger_extra_blame_args = "-wMC"
+      vim.g.git_messenger_floating_win_opts = { border = "single" }
     end,
   },
 
