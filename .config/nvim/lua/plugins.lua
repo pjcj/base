6ed94aca2a3b505c9c9ad7d2aa2eaa77aebccee9
Paul Johnson <paul@pjcj.net>
2023-10-07 02:46:25 +0200

    Use new perl treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -102,8 +102,8 @@ local plugins = {
         highlight = {
           enable = true,
           disable = {
-            "perl",
-            "sh",
+            -- "perl",
+            -- "sh",
             "gitcommit",
           },
         },
@@ -126,6 +126,15 @@ local plugins = {
         },
         autotag = {
           enable = true,
+        },
+      }
+      local parser_config =
+        require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.perl = {
+        install_info = {
+          url = "https://github.com/tree-sitter-perl/tree-sitter-perl",
+          revision = "release",
+          files = { "src/parser.c", "src/scanner.c" },
         },
       }
     end,
