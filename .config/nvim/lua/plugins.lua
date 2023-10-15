f81879ab22a715acedac889496bb9611cc1f4047
Paul Johnson <paul@pjcj.net>
2023-10-15 15:42:27 +0200

    Try to specify model

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1103,10 +1103,18 @@ local plugins = {
     config = function()
       require("chatgpt").setup {
         openai_params = {
-          model = "gpt-4",
+          model = "gpt-4-32k",
           frequency_penalty = 0,
           presence_penalty = 0,
-          max_tokens = 3000,
+          max_tokens = 2000,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "gpt-4-32k",
+          frequency_penalty = 0,
+          presence_penalty = 0,
           temperature = 0,
           top_p = 1,
           n = 1,
