4e00434214a4b9bc413114b65219f2f2c73abd37
Paul Johnson <paul@pjcj.net>
2023-10-28 19:40:29 +0200

    Remove default config parts

--- .config/tmux-powerline/themes/pjcj.sh
+++ .config/tmux-powerline/themes/pjcj.sh
@@ -6,53 +6,16 @@ export TMUX_POWERLINE_STATUS_INTERVAL=5
 export TMUX_POWERLINE_STATUS_RIGHT_LENGTH=160
 export TMUX_POWERLINE_SEG_TIME_FORMAT="%R %Z"
 
-if patched_font_in_use; then
-  TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
-  # TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
-  TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
-  TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
-else
-  TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
-  # TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
-  TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
-  TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
-fi
+TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
+TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
+# TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
+# TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
 
 : "${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:=#0e3c49}"
 : "${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:=#ced4d4}"
 
 : "${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:=$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}"
 : "${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:=$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}"
-
-# See man tmux.conf for additional formatting options for the status line.
-# The `format regular` and `format inverse` functions are provided as conveniences
-
-if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_CURRENT" ]; then
-  TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
-    "#[$(format inverse)]"
-    "$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
-    " #I#F "
-    "$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
-    " #W "
-    "#[$(format regular)]"
-    "$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
-  )
-fi
-
-if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_STYLE" ]; then
-  TMUX_POWERLINE_WINDOW_STATUS_STYLE=(
-    "$(format regular)"
-  )
-fi
-
-if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_FORMAT" ]; then
-  TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
-    "#[$(format regular)]"
-    "  #I#{?window_flags,#F, } "
-    "$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
-    " #W "
-  )
-fi
 
 # Format: segment_name background_color foreground_color [non_default_separator] [separator_background_color] [separator_foreground_color] [spacing_disable] [separator_disable]
 #
