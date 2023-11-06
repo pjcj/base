0ec41886dfc084c5478ffd3a793c55de2d8a315b
Paul Johnson <paul@pjcj.net>
2023-11-06 14:09:53 +0100

    Update markdown plugin config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1529,8 +1529,13 @@ local plugins = {
 
   {
     "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && yarn install",
-    ft = "markdown",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_echo_preview_url = 1
+    end,
+    ft = { "markdown" },
   },
 
   {
