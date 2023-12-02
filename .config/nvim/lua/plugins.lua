edafe0d0020db4cb18c299cffc7cc60bd4ed3f07
Paul Johnson <paul@pjcj.net>
2023-12-02 15:06:07 +0100

    Use default ChatGPT values

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1133,23 +1133,23 @@ local plugins = {
     },
     config = function()
       require("chatgpt").setup {
-        openai_params = {
-          model = "gpt-4-32k",
-          frequency_penalty = 0,
-          presence_penalty = 0,
-          max_tokens = 2000,
-          temperature = 0,
-          top_p = 1,
-          n = 1,
-        },
-        openai_edit_params = {
-          model = "gpt-4-32k",
-          frequency_penalty = 0,
-          presence_penalty = 0,
-          temperature = 0,
-          top_p = 1,
-          n = 1,
-        },
+        -- openai_params = {
+        --   model = "gpt-4-32k",
+        --   frequency_penalty = 0,
+        --   presence_penalty = 0,
+        --   max_tokens = 2000,
+        --   temperature = 0,
+        --   top_p = 1,
+        --   n = 1,
+        -- },
+        -- openai_edit_params = {
+        --   model = "gpt-4-32k",
+        --   frequency_penalty = 0,
+        --   presence_penalty = 0,
+        --   temperature = 0,
+        --   top_p = 1,
+        --   n = 1,
+        -- },
       }
     end,
   },
