6d0daf7ba19d5d31eb7f4365ba096e29732ec9ae
Paul Johnson <paul@pjcj.net>
2024-01-11 18:43:42 +0100

    Reduce big file size

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -877,7 +877,7 @@ local plugins = {
       }
 
       local buf_is_big = function(bufnr)
-        local max_filesize = 200 * 1024 -- 200 KB
+        local max_filesize = 120 * 1024 -- 120 KB
         local ok, stats =
           pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
         if ok and stats and stats.size > max_filesize then
