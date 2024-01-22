54a2295e9e04e4f3783391250939994416763ff3
Paul Johnson <paul@pjcj.net>
2024-01-22 09:46:38 +0100

    Remove old config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1176,27 +1176,6 @@ local plugins = {
       --   },
       -- })
 
-      require("cmp_dictionary").setup({
-        dic = {
-          ["*"] = { "/usr/share/dict/words" },
-          -- ["lua"] = "path/to/lua.dic",
-          -- ["javascript,typescript"] = { "path/to/js.dic", "path/to/js2.dic" },
-          -- filename = {
-          --   ["xmake.lua"] = { "path/to/xmake.dic", "path/to/lua.dic" },
-          -- },
-          -- filepath = {
-          --   ["%.tmux.*%.conf"] = "path/to/tmux.dic"
-          -- },
-        },
-        -- The following are default values, so you don't need to write them if
-        -- you don't want to change them
-        -- exact = 2,
-        -- first_case_insensitive = false,
-        -- async = false,
-        -- capacity = 5,
-        -- debug = false,
-      })
-
       -- Use buffer source for `/`.
       cmp.setup.cmdline("/", {
         mapping = cmp.mapping.preset.cmdline(),
