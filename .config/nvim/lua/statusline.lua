ba7e95356fe578a952878e8e4d7067e2449020b7
Paul Johnson <paul@pjcj.net>
2024-08-10 18:56:14 +0200

    Format statusline code

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,15 +1,15 @@
-local lsp           = require "feline.providers.lsp"
-local vi_mode_utils = require "feline.providers.vi_mode"
-local l             = require "local_defs"
-local c             = l.colour
+local lsp = require("feline.providers.lsp")
+local vi_mode_utils = require("feline.providers.vi_mode")
+local l = require("local_defs")
+local c = l.colour
 
 local components = {
-  active   = {},
+  active = {},
   inactive = {},
 }
 
-table.insert(components.active,   {})
-table.insert(components.active,   {})
+table.insert(components.active, {})
+table.insert(components.active, {})
 table.insert(components.inactive, {})
 
 table.insert(components.active[1], {
@@ -17,13 +17,13 @@ table.insert(components.active[1], {
   hl = function()
     return {
       name = vi_mode_utils.get_mode_highlight_name(),
-      bg   = vi_mode_utils.get_mode_color(),
+      bg = vi_mode_utils.get_mode_color(),
     }
   end,
   left_sep = function()
     return {
       str = " ",
-      hl  = { bg = vi_mode_utils.get_mode_color() },
+      hl = { bg = vi_mode_utils.get_mode_color() },
     }
   end,
   right_sep = {
@@ -31,13 +31,13 @@ table.insert(components.active[1], {
     function()
       return {
         str = " ",
-        hl  = { bg = vi_mode_utils.get_mode_color() },
+        hl = { bg = vi_mode_utils.get_mode_color() },
       }
     end,
     function()
       return {
         str = "right_filled",
-        hl  = { bg = c.base02, fg = vi_mode_utils.get_mode_color() },
+        hl = { bg = c.base02, fg = vi_mode_utils.get_mode_color() },
       }
     end,
   },
@@ -45,13 +45,15 @@ table.insert(components.active[1], {
 })
 
 table.insert(components.active[1], {
-  provider  = function() return vim.fn.getcwd():match("([^/]+)$") .. " " end,
-  hl        = { bg = c.base02, fg = c.base01 },
-  left_sep  = { str = " ", hl = { bg = c.base02 } },
+  provider = function()
+    return vim.fn.getcwd():match("([^/]+)$") .. " "
+  end,
+  hl = { bg = c.base02, fg = c.base01 },
+  left_sep = { str = " ", hl = { bg = c.base02 } },
   right_sep = {
     {
       str = "right_filled",
-      hl  = { bg = c.base05, fg = c.base02 },
+      hl = { bg = c.base05, fg = c.base02 },
     },
     " ",
   },
@@ -60,29 +62,29 @@ table.insert(components.active[1], {
 
 local file_info = {
   provider = {
-    name   = "file_info",
-    opts   = {
+    name = "file_info",
+    opts = {
       type = "relative",
       file_modified_icon = "",
     },
-    hl        = { bg = c.base02 },
-    left_sep  = { str = " ", hl = { bg = c.base02 } },
+    hl = { bg = c.base02 },
+    left_sep = { str = " ", hl = { bg = c.base02 } },
     right_sep = {
       str = "right_filled",
-      hl  = { bg = c.base05, fg = c.base02 },
+      hl = { bg = c.base05, fg = c.base02 },
     },
   },
-  short_provider  = {
-    name  = "file_info",
-    opts  = {
+  short_provider = {
+    name = "file_info",
+    opts = {
       type = "relative-short",
       file_modified_icon = "",
     },
-    hl        = { bg = c.base02 },
-    left_sep  = { str = " ", hl = { bg = c.base02 } },
+    hl = { bg = c.base02 },
+    left_sep = { str = " ", hl = { bg = c.base02 } },
     right_sep = {
       str = "right_filled",
-      hl  = { bg = c.base05, fg = c.base02 },
+      hl = { bg = c.base05, fg = c.base02 },
     },
   },
 }
@@ -95,20 +97,20 @@ table.insert(components.active[1], {
 })
 table.insert(components.active[1], {
   provider = "git_diff_added",
-  hl       = { fg = c.rgreen },
-  icon     = " +",
+  hl = { fg = c.rgreen },
+  icon = " +",
 })
 
 table.insert(components.active[1], {
   provider = "git_diff_changed",
-  hl       = { fg = c.yellow },
-  icon     = " ~",
+  hl = { fg = c.yellow },
+  icon = " ~",
 })
 
 table.insert(components.active[1], {
-  provider  = "git_diff_removed",
-  hl        = { fg = c.red },
-  icon      = " -",
+  provider = "git_diff_removed",
+  hl = { fg = c.red },
+  icon = " -",
   right_sep = {
     { str = " ", always_visible = true },
     {
@@ -120,20 +122,22 @@ table.insert(components.active[1], {
 })
 
 table.insert(components.active[1], {
-  provider      = "lsp_client_names",
-  hl            = { bg = c.base02, fg = c.base01 },
-  left_sep      = { str = " ", hl = { bg = c.base02 } },
-  right_sep     = { str = " ", hl = { bg = c.base02 } },
+  provider = "lsp_client_names",
+  hl = { bg = c.base02, fg = c.base01 },
+  left_sep = { str = " ", hl = { bg = c.base02 } },
+  right_sep = { str = " ", hl = { bg = c.base02 } },
   truncate_hide = true,
 })
 
 table.insert(components.active[1], {
-  provider      = function() return require "lsp-progress".progress() .. " " end,
-  hl            = { bg = c.base02, fg = c.base01 },
+  provider = function()
+    return require("lsp-progress").progress() .. " "
+  end,
+  hl = { bg = c.base02, fg = c.base01 },
   right_sep = {
     {
       str = "right_filled",
-      hl  = { bg = c.base05, fg = c.base02 },
+      hl = { bg = c.base05, fg = c.base02 },
     },
   },
   truncate_hide = true,
@@ -143,51 +147,63 @@ local severity = vim.diagnostic.severity
 
 table.insert(components.active[1], {
   provider = "diagnostic_errors",
-  enabled  = function() return lsp.diagnostics_exist(severity.ERROR) end,
-  hl       = { bg = c.base05, fg = c.c_red },
-  icon     = " E ",
+  enabled = function()
+    return lsp.diagnostics_exist(severity.ERROR)
+  end,
+  hl = { bg = c.base05, fg = c.c_red },
+  icon = " E ",
 })
 
 table.insert(components.active[1], {
   provider = "diagnostic_warnings",
-  enabled  = function() return lsp.diagnostics_exist(severity.WARN) end,
-  hl       = { bg = c.base05, fg = c.c_yellow },
-  icon     = " W ",
+  enabled = function()
+    return lsp.diagnostics_exist(severity.WARN)
+  end,
+  hl = { bg = c.base05, fg = c.c_yellow },
+  icon = " W ",
 })
 
 table.insert(components.active[1], {
   provider = "diagnostic_info",
-  enabled  = function() return lsp.diagnostics_exist(severity.INFO) end,
-  hl       = { bg = c.base05, fg = c.c_sky },
-  icon     = " I ",
+  enabled = function()
+    return lsp.diagnostics_exist(severity.INFO)
+  end,
+  hl = { bg = c.base05, fg = c.c_sky },
+  icon = " I ",
 })
 
 table.insert(components.active[1], {
   provider = "diagnostic_hints",
-  enabled  = function() return lsp.diagnostics_exist(severity.HINT) end,
-  hl       = { bg = c.base05, fg = c.blue },
-  icon     = " H ",
+  enabled = function()
+    return lsp.diagnostics_exist(severity.HINT)
+  end,
+  hl = { bg = c.base05, fg = c.blue },
+  icon = " H ",
 })
 
 table.insert(components.active[1], {
-  provider  = " ",
-  hl        = { bg = c.base05 },
+  provider = " ",
+  hl = { bg = c.base05 },
   right_sep = {
     str = "right_filled",
     hl = { bg = c.base02, fg = c.base05 },
   },
 })
 
-local navic = require "nvim-navic"
+local navic = require("nvim-navic")
 table.insert(components.active[1], {
-  provider      = function() return navic.get_location() .. " " end,
-  enabled       = function() return navic.is_available() end,
-  hl            = { bg = c.base02, fg = "yellow" },
-  left_sep  = { str = " ", hl = { bg = c.base02 } },
+  provider = function()
+    return navic.get_location() .. " "
+  end,
+  enabled = function()
+    return navic.is_available()
+  end,
+  hl = { bg = c.base02, fg = "yellow" },
+  left_sep = { str = " ", hl = { bg = c.base02 } },
   right_sep = {
     {
       str = "right_filled",
-      hl  = { bg = c.base05, fg = c.base02 },
+      hl = { bg = c.base05, fg = c.base02 },
     },
     " ",
   },
@@ -209,32 +225,34 @@ end
 
 table.insert(components.active[2], {
   provider = file_osinfo,
-  hl       = { bg = c.base02, fg = c.base01 },
+  hl = { bg = c.base02, fg = c.base01 },
   left_sep = {
     { str = "left_filled", hl = { bg = c.base05, fg = c.base02 } },
-    { str = " ",           hl = { bg = c.base02                } },
+    { str = " ", hl = { bg = c.base02 } },
   },
   truncate_hide = true,
 })
 
 table.insert(components.active[2], {
   provider = "file_type",
-  hl       = { bg = c.base02, fg = c.base01 },
+  hl = { bg = c.base02, fg = c.base01 },
   left_sep = { str = " ", hl = { bg = c.base02 } },
   truncate_hide = true,
 })
 
 table.insert(components.active[2], {
-  provider  = "file_encoding",
-  hl        = { bg = c.base02, fg = c.base01 },
-  left_sep  = { str = " ", hl = { bg = c.base02, fg = c.base05 } },
-  right_sep = { str = " ", hl = { bg = c.base02                } },
+  provider = "file_encoding",
+  hl = { bg = c.base02, fg = c.base01 },
+  left_sep = { str = " ", hl = { bg = c.base02, fg = c.base05 } },
+  right_sep = { str = " ", hl = { bg = c.base02 } },
   truncate_hide = true,
 })
 
 table.insert(components.active[2], {
   provider = "file_size",
-  enabled  = function() return vim.fn.getfsize(vim.fn.expand("%:p")) > 0 end,
+  enabled = function()
+    return vim.fn.getfsize(vim.fn.expand("%:p")) > 0
+  end,
   left_sep = {
     { str = "left_filled", hl = { bg = c.base02, fg = c.base05 } },
     " ",
@@ -247,32 +265,32 @@ table.insert(components.active[2], {
   hl = function()
     return {
       name = vi_mode_utils.get_mode_highlight_name(),
-      bg   = vi_mode_utils.get_mode_color(),
+      bg = vi_mode_utils.get_mode_color(),
     }
   end,
   left_sep = {
     function()
       return {
         str = "left_filled",
-        hl  = { bg = c.base05, fg = vi_mode_utils.get_mode_color() },
+        hl = { bg = c.base05, fg = vi_mode_utils.get_mode_color() },
       }
     end,
     function()
       return {
         str = " ",
-        hl  = { bg = vi_mode_utils.get_mode_color() },
+        hl = { bg = vi_mode_utils.get_mode_color() },
       }
     end,
   },
 })
 
 table.insert(components.inactive[1], {
-  provider  = "file_type",
-  hl        = { fg = "white", bg = "oceanblue", style = "bold" },
-  left_sep  = { str = " ", hl = { fg = "NONE", bg = "oceanblue" } },
+  provider = "file_type",
+  hl = { fg = "white", bg = "oceanblue", style = "bold" },
+  left_sep = { str = " ", hl = { fg = "NONE", bg = "oceanblue" } },
   right_sep = {
     { str = " ", hl = { fg = "NONE", bg = "oceanblue" } },
-    "right_filled"
+    "right_filled",
   },
 })
 
@@ -284,64 +302,64 @@ table.insert(components.inactive[1], {
 })
 
 local colours = {
-  bg        = c.base05,
-  black     = c.base05,
-  yellow    = c.yellow,
-  cyan      = c.cyan,
+  bg = c.base05,
+  black = c.base05,
+  yellow = c.yellow,
+  cyan = c.cyan,
   oceanblue = c.dblue,
-  green     = c.green,
-  orange    = c.orange,
-  violet    = c.violet,
-  magenta   = c.magenta,
-  white     = c.base2,
-  fg        = c.base2,
-  skyblue   = c.blue,
-  red       = c.red,
+  green = c.green,
+  orange = c.orange,
+  violet = c.violet,
+  magenta = c.magenta,
+  white = c.base2,
+  fg = c.base2,
+  skyblue = c.blue,
+  red = c.red,
 }
 
 local separators = {
-  vertical_bar       = "┃",
-  vertical_bar_thin  = "│",
-  left               = "",
-  right              = "",
-  block              = "█",
-  left_filled        = "",
-  right_filled       = "",
-  slant_left         = "",
-  slant_left_thin    = "",
-  slant_right        = "",
-  slant_right_thin   = "",
-  slant_left_2       = "",
-  slant_left_2_thin  = "",
-  slant_right_2      = "",
+  vertical_bar = "┃",
+  vertical_bar_thin = "│",
+  left = "",
+  right = "",
+  block = "█",
+  left_filled = "",
+  right_filled = "",
+  slant_left = "",
+  slant_left_thin = "",
+  slant_right = "",
+  slant_right_thin = "",
+  slant_left_2 = "",
+  slant_left_2_thin = "",
+  slant_right_2 = "",
   slant_right_2_thin = "",
-  left_rounded       = "",
-  left_rounded_thin  = "",
-  right_rounded      = "",
+  left_rounded = "",
+  left_rounded_thin = "",
+  right_rounded = "",
   right_rounded_thin = "",
-  circle             = "●",
+  circle = "●",
 }
 
 local vi_mode_colors = {
-  NORMAL        = c.dgreen,
-  OP            = c.dgreen,
-  INSERT        = "red",
-  VISUAL        = "skyblue",
-  BLOCK         = "skyblue",
-  REPLACE       = "violet",
+  NORMAL = c.dgreen,
+  OP = c.dgreen,
+  INSERT = "red",
+  VISUAL = "skyblue",
+  BLOCK = "skyblue",
+  REPLACE = "violet",
   ["V-REPLACE"] = "violet",
-  ENTER         = "cyan",
-  MORE          = "cyan",
-  SELECT        = "orange",
-  COMMAND       = c.dgreen,
-  SHELL         = c.dgreen,
-  TERM          = c.dgreen,
-  NONE          = "yellow",
+  ENTER = "cyan",
+  MORE = "cyan",
+  SELECT = "orange",
+  COMMAND = c.dgreen,
+  SHELL = c.dgreen,
+  TERM = c.dgreen,
+  NONE = "yellow",
 }
 
-require "feline".setup {
-  theme          = colours,
-  separators     = separators,
-  components     = components,
+require("feline").setup({
+  theme = colours,
+  separators = separators,
+  components = components,
   vi_mode_colors = vi_mode_colors,
-}
+})
