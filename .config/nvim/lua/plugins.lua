ad3cc8fdea5d0b01e309762bc71ce55b34113fc7
Paul Johnson <paul@pjcj.net>
2025-01-19 19:05:24 +0100

    Update lazy initialisation

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,14 +5,24 @@ vim.opt.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.fn.isdirectory(lazypath) then
-  vim.fn.system({
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
+    lazyrepo,
     lazypath,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
