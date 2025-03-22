a1d572804d5ed09fe23b2e841ac5c187e1be7d5e
Paul Johnson <paul@pjcj.net>
2025-03-22 18:18:32 +0100

    Add user and group to status

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -18,3 +18,17 @@ Status:children_add(function(self)
     return ""
   end
 end, 3300, Status.LEFT)
+
+Status:children_add(function()
+  local h = cx.active.current.hovered
+  if h == nil or ya.target_family() ~= "unix" then
+    return ""
+  end
+
+  return ui.Line({
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+    ":",
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+    " ",
+  })
+end, 500, Status.RIGHT)
