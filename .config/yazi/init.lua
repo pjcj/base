38eab85b46109790050d6db97a782b6e67473c15
Paul Johnson <paul@pjcj.net>
2025-03-22 18:15:20 +0100

    Add yazi link information to status line

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -9,3 +9,12 @@ function Linemode:size_and_mtime()
   local size = self._file:size()
   return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
+
+Status:children_add(function(self)
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
+end, 3300, Status.LEFT)
