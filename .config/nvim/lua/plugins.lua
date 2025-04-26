ab6c1ac0e052958fe671c33115aa36bdd73efdf3
Paul Johnson <paul@pjcj.net>
2025-04-26 17:15:50 +0200

    Remove obsolete code

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2280,23 +2280,3 @@ local opts = {
 
 require("lazy").setup(plugins, opts)
 require("lsp").setup_servers()
-
--- packer.startup(function(use)
---   use "mfussenegger/nvim-dap"
---   -- use {
---   --   "Pocco81/DAPInstall.nvim",
---   --   config = function()
---   --     local dap_install = require "dap-install"
---
---   --     dap_install.setup {
---   --       installation_path = vim.fn.stdpath "data" .. "/dapinstall/",
---   --     }
---
---   --     local dbg_list =
---   --       require "dap-install.api.debuggers".get_installed_debuggers()
---
---   --     for _, debugger in ipairs(dbg_list) do
---   --       dap_install.config(debugger)
---   --     end
---   --   end,
---   -- }
