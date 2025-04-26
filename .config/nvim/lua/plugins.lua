fe4ebdcea0071a280d517a5d66a74c31beb8357d
Paul Johnson <paul@pjcj.net>
2025-04-26 16:40:53 +0200

    Turn off render-markdown except for Avante

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1756,9 +1756,9 @@ local plugins = {
         -- Make sure to set this up properly if you have lazy=true
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
-          file_types = { "markdown", "Avante" },
+          file_types = { "Avante" },
         },
-        ft = { "markdown", "Avante" },
+        ft = { "Avante" },
       },
     },
     opts = {
