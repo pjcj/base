8863efc25f366b212168f76eb14bd1ad4e11f7c0
Paul Johnson <paul@pjcj.net>
2025-05-24 20:39:32 +0200

    Tidy avante config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1615,18 +1615,16 @@ local plugins = {
     "yetone/avante.nvim",
     enabled = vim.env.ENABLE_AI_PLUGINS ~= nil,
     event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
+    version = false, -- never set this to "*"
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      -- the below dependencies are optional
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "nvim-tree/nvim-web-devicons",
       "zbirenbaum/copilot.lua", -- for providers='copilot'
       "ravitemer/mcphub.nvim",
       {
@@ -1647,7 +1645,7 @@ local plugins = {
         },
       },
       {
-        -- Make sure to set this up properly if you have lazy=true
+        -- be sure to set this up properly if you have lazy=true
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
           file_types = { "Avante" },
@@ -1672,13 +1670,13 @@ local plugins = {
         max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
         --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
       },
-      -- system_prompt as function ensures LLM always has latest MCP server state
-      -- This is evaluated for every message, even in existing chats
+      -- system_prompt as function ensures LLM always has latest MCP server
+      -- state - evaluated for every message, even in existing chats.
       system_prompt = function()
         local hub = require("mcphub").get_hub_instance()
         return hub:get_active_servers_prompt()
       end,
-      -- Using function prevents requiring mcphub before it's loaded
+      -- using a function prevents requiring mcphub before it's loaded
       custom_tools = function()
         return {
           require("mcphub.extensions.avante").mcp_tool(),
@@ -1687,7 +1685,6 @@ local plugins = {
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   },
 
   {
