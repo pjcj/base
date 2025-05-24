df4a4031901ef93025d6bfde1df9a66e613663fd
Paul Johnson <paul@pjcj.net>
2025-05-24 23:53:28 +0200

    Disable drag and drop for image pasting
    This was causing warnings when just pasting text.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1634,11 +1634,10 @@ local plugins = {
           default = {
             embed_image_as_base64 = false,
             prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
+            -- drag_and_drop = {
+            --   insert_mode = true,
+            -- },
+            use_absolute_path = true, -- required for Windows
           },
         },
       },
