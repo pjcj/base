a10f330dc7cd748daab20c8a1d711f7b8d7408e2
Paul Johnson <paul@pjcj.net>
2025-06-03 22:31:20 +0200

    Change mapping to get ghost text

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1009,7 +1009,7 @@ local plugins = {
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
           }),
-          ["<C-Right>"] = cmp.mapping(function(fallback)
+          ["<C-S-Right>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               local gt = cmp.core.view.ghost_text_view
               local c = require("cmp.config").get().experimental.ghost_text
