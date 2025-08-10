d6271328dfe5cfb32d79dea84492926a91e8e027
Paul Johnson <paul@pjcj.net>
2025-08-10 22:50:18 +0200

    Remove chatgpt plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1142,39 +1142,6 @@ local plugins = {
     end,
   },
   { "AndreM222/copilot-lualine" },
-  -- {
-  --   "jackMort/ChatGPT.nvim",
-  --   enabled = vim.env.ENABLE_AI_PLUGINS ~= nil,
-  --   event = "VeryLazy",
-  --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "folke/trouble.nvim",
-  --     "nvim-telescope/telescope.nvim",
-  --   },
-  --   config = function()
-  --     require("chatgpt").setup({
-  --       -- openai_params = {
-  --       --   model = "gpt-4-32k",
-  --       --   frequency_penalty = 0,
-  --       --   presence_penalty = 0,
-  --       --   max_tokens = 2000,
-  --       --   temperature = 0,
-  --       --   top_p = 1,
-  --       --   n = 1,
-  --       -- },
-  --       -- openai_edit_params = {
-  --       --   model = "gpt-4-32k",
-  --       --   frequency_penalty = 0,
-  --       --   presence_penalty = 0,
-  --       --   temperature = 0,
-  --       --   top_p = 1,
-  --       --   n = 1,
-  --       -- },
-  --     })
-  --   end,
-  -- },
-
   {
     "olimorris/codecompanion.nvim",
     version = "*",
