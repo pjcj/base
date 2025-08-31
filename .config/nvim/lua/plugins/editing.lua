720fb3779c554dc27474ef10b464edd7927627c6
Paul Johnson <paul@pjcj.net>
2025-08-31 15:45:11 +0200

    Format lua

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -24,10 +24,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "<F53>", -- option-f5
+            init_selection = "<F53>",    -- option-f5
             scope_incremental = "<F55>", -- option-f7
-            node_incremental = "<F53>", -- option-f5
-            node_decremental = "<F54>", -- option-f6
+            node_incremental = "<F53>",  -- option-f5
+            node_decremental = "<F54>",  -- option-f6
           },
         },
         refactor = {
@@ -51,16 +51,16 @@ return {
       })
       require("treesitter-context").setup({
         enable = true,
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        trim_scope = "outer",     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor",          -- Line used to calculate context. Choices: 'cursor', 'topline'
         -- Separator between context and content. Should be a single character string, like '-'.
         -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
         separator = nil,
-        zindex = 20, -- The Z-index of the context window
+        zindex = 20,     -- The Z-index of the context window
         on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
       })
     end,
