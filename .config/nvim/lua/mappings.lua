418bd9ada2864ebb60a9a71300b8d658cc81c6f4
Paul Johnson <paul@pjcj.net>
2025-11-02 19:51:30 +0100

    Add marks.nvim mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1568,24 +1568,22 @@ wk.add({
   },
 })
 
--- marks.nvim
--- mx              Set mark x
--- m,              Set the next available alphabetical (lowercase) mark
--- m;              Toggle the next available mark at the current line
--- dmx             Delete mark x
--- dm-             Delete all marks on the current line
--- dm<space>       Delete all marks in the current buffer
--- m]              Move to next mark
--- m[              Move to previous mark
--- m:              Preview mark. This will prompt you for a specific mark to
---                 preview; press <cr> to preview the next mark.
--- m[0-9]          Add a bookmark from bookmark group[0-9].
--- dm[0-9]         Delete all bookmarks from bookmark group[0-9].
--- m}              Move to the next bookmark having the same type as the
---                 bookmark under the cursor. Works across buffers.
--- m{              Move to the previous bookmark having the same type as the
---                 bookmark under the cursor. Works across buffers.
--- dm=             Delete the bookmark under the cursor.
+-- marks.nvim mappings
+wk.add({
+  { "m", group = "marks (mx=set, m[0-9]=bookmark)" },
+  { "m,", desc = "set next available mark" },
+  { "m;", desc = "toggle next available mark" },
+  { "m]", desc = "next mark" },
+  { "m[", desc = "previous mark" },
+  { "m:", desc = "preview mark" },
+  { "m}", desc = "next bookmark (same type)" },
+  { "m{", desc = "previous bookmark (same type)" },
+
+  { "dm", group = "delete marks (dmx=mark, dm[0-9]=group)" },
+  { "dm-", desc = "delete marks on line" },
+  { "dm<space>", desc = "delete marks in buffer" },
+  { "dm=", desc = "delete bookmark under cursor" },
+})
 
 vim.keymap.set("ia", ",,", "=>", { desc = "arrow abbreviation" })
 
