ce1558626ebce63aebcda5e5b00e7d579aa68308
Paul Johnson <paul@pjcj.net>
2025-11-14 14:04:55 +0100

    Explicitly disable ALE for lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -109,12 +109,12 @@ local plugins = {
       vim.g.ale_linters_explicit = 0
       vim.g.ale_disable_lsp = "auto"
       vim.g.ale_use_neovim_diagnostics_api = 1
-      -- Disable ALE virtual text to use tiny-inline-diagnostic instead
       vim.g.ale_virtualtext_cursor = 0
       vim.g.ale_linters = {
         -- go = { "gofmt", "golint", "gopls", "govet", "golangci-lint" },
         go = {},
         html = {},
+        lua = {},
         markdown = {},
         perl = {},
         sh = {},
